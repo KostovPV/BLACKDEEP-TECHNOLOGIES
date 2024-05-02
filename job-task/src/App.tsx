972ed31ab0322e7fdda09be9ea9e5e4550f8994f
KostovPV <106186518+KostovPV@users.noreply.github.com>
2024-05-02 22:45:43 +0300

    Form integration started

--- job-task/src/App.tsx
+++ job-task/src/App.tsx
@@ -1,17 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Container } from '@chakra-ui/react'
+import { Box, FormControl, FormHelperText, FormLabel, Input, Button, Center } from '@chakra-ui/react'
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <Center >
+            <Box maxWidth={780} minWidth={400} py='20' >
+                <form>
+                    <FormControl isRequired mb='30px'>
+                        <FormLabel fontSize={'xl'} textAlign={'center'}>Register account</FormLabel>
+                        <Input type='text' name='first_name' placeholder='Enter First Name' />
+                    </FormControl>
 
-  return (
-    <Container>
-      Hello
-    </Container>
-  )
+                    <FormControl isRequired mb='30px'>
+                        <Input type='text' name='last_name' placeholder='Enter Last Name' />
+                    </FormControl>
+                    
+                    <FormControl isRequired mb='30px'>
+                        <Input type='password' name='pass' placeholder='Password' />
+                    </FormControl>
+                    
+                    <FormControl isRequired mb='30px'>
+                    <Input type='password' name='re-pass' placeholder='Repeat Password' />
+                    </FormControl> 
+                    
+                    <Button colorScheme='teal' type='submit' mt={4}>
+                        Next
+                    </Button>
+                </form>
+            </Box>
+        </Center >
+    )
 }
 
 export default App
