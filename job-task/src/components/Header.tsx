01370f84d67f861c4deaf8288e2baaf207e14553
KostovPV <106186518+KostovPV@users.noreply.github.com>
2024-05-06 10:51:34 +0300

    Cnahged the home page logic to be with react-hook-form

--- job-task/src/components/Header.tsx
+++ job-task/src/components/Header.tsx
@@ -1,4 +1,5 @@
-import {  Button, Flex, HStack, Heading, Spacer, Text } from "@chakra-ui/react";
+import { Button, Flex, HStack, Heading, Spacer, Text } from "@chakra-ui/react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Header() {
@@ -10,18 +11,30 @@ export default function Header() {
         navigate('/');
     };
 
+    // Listen for the popstate event (back button pressed)
+    useEffect(() => {
+        const handlePopState = () => {
+            handleLogout();
+        };
+
+        window.addEventListener('popstate', handlePopState);
+
+        return () => {
+            window.removeEventListener('popstate', handlePopState);
+        };
+    }, [navigate]);
+
     return (
         <Flex as="nav" p="10px" alignItems="center" bg="black" color="rgb(245, 207, 26)">
             <Heading as="h1">Job Task</Heading>
             <Spacer />
-            
             <Text paddingRight={2} color="rgb(245, 207, 26)" px="2" py="1" borderRadius="md">Welcome to code challenge</Text>
             {userData.first_name && (
                 <HStack spacing="20px">
                     <Text color="rgb(245, 207, 26)" px="2" py="1" borderRadius="md">{userData.first_name}</Text>
-                    <Button  color="rgb(245, 207, 26)" onClick={handleLogout}>Logout</Button>
+                    <Button color="rgb(245, 207, 26)" onClick={handleLogout}>Logout</Button>
                 </HStack>
             )}
         </Flex>
-    )
+    );
 }
--- job-task/src/pages/Home/Home.tsx
+++ job-task/src/pages/Home/Home.tsx
@@ -1,8 +1,9 @@
-import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
-import { Box, FormControl, FormLabel, Input, Button, Center, Checkbox, FormErrorMessage } from '@chakra-ui/react';
-import { useNavigate } from 'react-router-dom';
+import { Box, FormControl, FormLabel, Button, Center, Input, FormErrorMessage, Checkbox } from '@chakra-ui/react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom'; 
 
-interface FormData {
+type FormFields = {
     first_name: string;
     last_name: string;
     password: string;
@@ -12,212 +13,190 @@ interface FormData {
 }
 
 function Home(): JSX.Element {
-    const [step, setStep] = useState<number>(1);
-    const [formData, setFormData] = useState<FormData>({
-        first_name: '',
-        last_name: '',
-        password: '',
-        confirm_password: '',
-        interests: [],
-        avatar: ''
-    });
-
-    const [errors, setErrors] = useState<Partial<FormData>>({});
-    const [interestError, setInterestError] = useState<string>('');
+    const { register, handleSubmit, formState: { errors } } = useForm<FormFields>();
+    const [showAvatarForm, setShowAvatarForm] = useState<boolean>(false);
+    const [selectedInterests, setSelectedInterests] = useState<string[]>([]);
+    const [showFirstBox, setShowFirstBox] = useState<boolean>(true); 
     const navigate = useNavigate();
 
     useEffect(() => {
-        // Clear user data when navigating back to Home
         localStorage.removeItem('userData');
     }, []);
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLInputElement>): void => {
-        const { name, value, type, checked } = e.target;
-        let newValue;
 
-        if (type === 'checkbox') {
-            if (checked) {
-                newValue = [...formData[name as keyof FormData], value];
-            } else {
-                newValue = (formData[name as keyof FormData] as string[]).filter((interest: string) => interest !== value);
+    const onSubmit: SubmitHandler<FormFields> = (data) => {
+        console.log("On submit executed");
+        console.log(errors);
+        console.log(data);
+
+        
+        localStorage.setItem('userData', JSON.stringify(data));
+        navigate('/profile');
+    }
+
+    const handleNext = () => {
+        handleSubmit((data) => {
+            if (Object.keys(errors).length === 0) {
+                setShowAvatarForm(true);
+                setShowFirstBox(false); 
             }
+        })();
+    }
+
+    const handleBack = () => {
+        setShowAvatarForm(false);
+        setShowFirstBox(true);
+    }
+
+    const handleCheckboxChange = (value: string) => {
+        if (selectedInterests.includes(value)) {
+            setSelectedInterests(selectedInterests.filter((interest) => interest !== value));
         } else {
-            newValue = value;
-        }
-
-        setFormData({ ...formData, [name]: newValue });
-    };
-
-
-
-
-    const validateStep1 = (): boolean => {
-        const { first_name, last_name, password, confirm_password, interests } = formData;
-        const newErrors: Partial<FormData> = {};
-
-        if (!first_name || first_name.length < 3) {
-            newErrors.first_name = 'First name must be at least 3 characters';
-        }
-
-        if (!last_name || last_name.length < 3) {
-            newErrors.last_name = 'Last name must be at least 3 characters';
-        }
-
-        if (!password || password.length < 6) {
-            newErrors.password = 'Password must be at least 6 characters';
-        }
-
-        if (password !== confirm_password) {
-            newErrors.confirm_password = 'Passwords do not match';
-        }
-
-        if (interests.length === 2) {
-            setInterestError('You can only select up to 2 interests');
-        } else {
-            setInterestError('');
-        }
-
-        setErrors(newErrors);
-        return Object.keys(newErrors).length === 0 && interests.length > 0 && interests.length <= 2;
-    };
-
-    const handleNext = (): void => {
-        if (validateStep1()) {
-            setStep((prevStep) => prevStep + 1);
-        }
-    };
-
-    const handleBack = (): void => {
-        setStep((prevStep) => prevStep - 1);
-    };
-
-    const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
-        e.preventDefault();
-        if (step === 1) {
-            if (validateStep1()) {
-                localStorage.setItem('userData', JSON.stringify(formData));
-                navigate('/profile');
+            if (selectedInterests.length < 2) {
+                setSelectedInterests([...selectedInterests, value]);
             }
-        } else if (step === 2) {
-            localStorage.setItem('userData', JSON.stringify(formData));
-            navigate('/profile');
         }
-    };
-
-
-
-
-
-
-
+    }
 
     return (
         <Center>
 
-            <Box
-                maxWidth={['100%', '60%', '480px', '480px']}
-                width="100%"
-                px={4}
-                pt={[6, 6, 10, 20]} // Reduced padding top on small devices
-                pb={6}
-            >
-                <form onSubmit={handleSubmit} style={{ display: step === 1 ? 'block' : 'none' }}>
-                    <FormControl isRequired mb={8} isInvalid={!!errors.first_name}>
-                    <FormLabel fontSize='2xl' textAlign='center' color="white">Register account</FormLabel>
-                        <Input type='text' name='first_name' placeholder='Enter First Name' onChange={handleChange}  color='rgba(255, 255, 255, 0.7)' _placeholder={{ color: 'rgba(255, 255, 255, 0.3)' }}/>
-                        <FormErrorMessage>{errors.first_name}</FormErrorMessage>
-                    </FormControl>
 
-                    <FormControl isRequired mb={8} isInvalid={!!errors.last_name}>
-                        <Input type='text' name='last_name' placeholder='Enter Last Name' onChange={handleChange} color='rgba(255, 255, 255, 0.7)' _placeholder={{ color: 'rgba(255, 255, 255, 0.3)' }}/>
-                        <FormErrorMessage>{errors.last_name}</FormErrorMessage>
-                    </FormControl>
+            <form onSubmit={handleSubmit(onSubmit)}  >
+                {showFirstBox && (
+                    <Box
+                        maxWidth={['100%', '60%', '480px', '480px']}
+                        width="100%"
+                        px={4}
+                        pt={[6, 6, 10, 20]} 
+                        pb={6}
 
-                    <FormControl isRequired mb={8} isInvalid={!!errors.password}>
-                        <Input type='password' name='password' placeholder='Password' onChange={handleChange} color='rgba(255, 255, 255, 0.7)' _placeholder={{ color: 'rgba(255, 255, 255, 0.3)' }}/>
-                        <FormErrorMessage>{errors.password}</FormErrorMessage>
-                    </FormControl>
+                    >
+                        <FormLabel fontSize='2xl' textAlign='center' color="white">Register account</FormLabel>
+                        <FormControl mb={8} isInvalid={!!errors.first_name}>
 
-                    <FormControl isRequired mb={8} isInvalid={!!errors.confirm_password}>
-                        <Input type='password' name='confirm_password' placeholder='Confirm Password' onChange={handleChange} color='rgba(255, 255, 255, 0.7)' _placeholder={{ color: 'rgba(255, 255, 255, 0.3)' }}/>
-                        <FormErrorMessage>{errors.confirm_password}</FormErrorMessage>
-                    </FormControl>
+                            <Input {...register("first_name", {
+                                required: "First name is required",
+                                minLength: { value: 4, message: "First name should be at least 4 characters long" }
+                            })} type='text' name='first_name' placeholder='Enter First Name' color='rgba(255, 255, 255, 0.7)' />
+                            <FormErrorMessage>{errors.first_name?.message}</FormErrorMessage>
+                        </FormControl>
 
-                    <FormLabel fontSize='xl' textAlign='center' color="white">Interests</FormLabel>
-                    <FormControl display="flex" alignItems="center" mb="40px">
-                        <Checkbox
-                            key={1}
-                            id={`field-ports-1`}
-                            name='interests'
-                            value='Sports'
-                            size="lg"
-                            onChange={handleChange}
-                            isChecked={formData.interests.includes('Sports')}
-                            isDisabled={formData.interests.length >= 2 && !formData.interests.includes('Sports')}
-                        />
-                        <FormLabel mb="0" ml="2" color="white">Sports</FormLabel>
-                        <Checkbox
-                            key={2}
-                            id={`field-music-2`}
-                            ml="2"
-                            name='interests'
-                            value='Music'
-                            size="lg"
-                            onChange={handleChange}
-                            isChecked={formData.interests.includes('Music')}
-                            isDisabled={formData.interests.length >= 2 && !formData.interests.includes('Music')}
-                        />
-                        <FormLabel mb="0" ml="2" color="white">Music</FormLabel>
-                        <Checkbox
-                            key={3}
-                            id={`field-dancing-3`}
-                            ml="2"
-                            name='interests'
-                            value='Dancing'
-                            size="lg"
-                            onChange={handleChange}
-                            isChecked={formData.interests.includes('Dancing')}
-                            isDisabled={formData.interests.length >= 2 && !formData.interests.includes('Dancing')}
-                        />
-                        <FormLabel mb="0" ml="2" color="white">Dancing</FormLabel>
-                        <Checkbox
-                            key={4}
-                            id={`field-games-4`}
-                            ml="2"
-                            name='interests'
-                            value='Games'
-                            size="lg"
-                            onChange={handleChange}
-                            isChecked={formData.interests.includes('Games')}
-                            isDisabled={formData.interests.length >= 2 && !formData.interests.includes('Games')}
-                        />
-                        <FormLabel mb="0" ml="2" color="white">Games</FormLabel>
-                    </FormControl>
-                    <FormErrorMessage>{interestError}</FormErrorMessage>
-                    <Button colorScheme='rgb(245, 207, 26)'
-                        bg='rgb(245, 207, 26)'
-                        type='button'
-                        onClick={handleNext}
-                        mt={4}>
-                        Next
-                    </Button>
+                        <FormControl mb={8} isInvalid={!!errors.last_name}>
 
-                </form>
+                            <Input {...register("last_name", {
+                                required: "Last name is required",
+                                minLength: { value: 4, message: "Last name should be at least 4 characters long" }
+                            })} type='text' name='last_name' placeholder='Enter Last Name' color='rgba(255, 255, 255, 0.7)' />
+                            <FormErrorMessage>{errors.last_name?.message}</FormErrorMessage>
+                        </FormControl>
 
-                <form onSubmit={handleSubmit} style={{ display: step === 2 ? 'block' : 'none' }}>
-                    <FormControl isRequired mb={8}>
-                        <FormLabel fontSize='xl' textAlign='center' color='rgba(255, 255, 255, 0.8)'>Avatar URL</FormLabel>
-                        <Input type='url' name='avatar' placeholder='Enter Image URL' onChange={handleChange} />
-                    </FormControl>
+                        <FormControl mb={8} isInvalid={!!errors.password}>
 
-                    <Button colorScheme='teal' type='button' onClick={handleBack} mr={2} mt={4}>
-                        Back
-                    </Button>
-                    <Button colorScheme='teal' type='submit' mt={4}>
-                        Submit
-                    </Button>
-                </form>
-            </Box>
+                            <Input {...register("password", {
+                                required: "Password is required",
+                                minLength: { value: 4, message: "Password should be at least 4 characters long" }
+                            })} type='password' name='password' placeholder='Enter Password' color='rgba(255, 255, 255, 0.7)' />
+                            <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
+                        </FormControl>
+
+                        <FormControl mb={8} isInvalid={!!errors.confirm_password}>
+
+                            <Input {...register("confirm_password", {
+                                required: "Confirm Password is required",
+                                minLength: { value: 4, message: "Confirm Password should be at least 4 characters long" }
+                            })} type='password' name='confirm_password' placeholder='Confirm Password' color='rgba(255, 255, 255, 0.7)' />
+                            <FormErrorMessage>{errors.confirm_password?.message}</FormErrorMessage>
+                        </FormControl>
+
+
+
+
+                        <FormLabel fontSize='xl' textAlign='center' color="white">Interests</FormLabel>
+                        <FormControl display="flex" alignItems="center" mb="40px">
+                            <Checkbox
+                                key={1}
+                                id={`field-ports-1`}
+                                name='interests'
+                                value='Sports'
+                                size="lg"
+                                onChange={() => handleCheckboxChange('Sports')}
+                                isChecked={selectedInterests.includes('Sports')}
+                                isDisabled={selectedInterests.length === 2 && !selectedInterests.includes('Sports')}
+                            />
+                            <FormLabel mb="0" ml="2" color="white">Sports</FormLabel>
+                            <Checkbox
+                                key={2}
+                                id={`field-music-2`}
+                                ml="2"
+                                name='interests'
+                                value='Music'
+                                size="lg"
+                                onChange={() => handleCheckboxChange('Music')}
+                                isChecked={selectedInterests.includes('Music')}
+                                isDisabled={selectedInterests.length === 2 && !selectedInterests.includes('Music')}
+                            />
+                            <FormLabel mb="0" ml="2" color="white">Music</FormLabel>
+                            <Checkbox
+                                key={3}
+                                id={`field-dancing-3`}
+                                ml="2"
+                                name='interests'
+                                value='Dancing'
+                                size="lg"
+                                onChange={() => handleCheckboxChange('Dancing')}
+                                isChecked={selectedInterests.includes('Dancing')}
+                                isDisabled={selectedInterests.length === 2 && !selectedInterests.includes('Dancing')}
+                            />
+                            <FormLabel mb="0" ml="2" color="white">Dancing</FormLabel>
+                            <Checkbox
+                                key={4}
+                                id={`field-games-4`}
+                                ml="2"
+                                name='interests'
+                                value='Games'
+                                size="lg"
+                                onChange={() => handleCheckboxChange('Games')}
+                                isChecked={selectedInterests.includes('Games')}
+                                isDisabled={selectedInterests.length === 2 && !selectedInterests.includes('Games')}
+                            />
+                            <FormLabel mb="0" ml="2" color="white">Games</FormLabel>
+                        </FormControl>
+                        <FormErrorMessage></FormErrorMessage>
+                        <Button colorScheme='rgb(245, 207, 26)'
+                            bg='rgb(245, 207, 26)'
+                            type='button'
+                            onClick={handleNext}
+                            mt={4}>
+                            Next
+                        </Button>
+                    </Box>
+                )
+                }
+                {showAvatarForm && (
+                    <Box mt={150}>
+
+                        <FormControl mb={8} isInvalid={!!errors.confirm_password}>
+
+                            <Input {...register("avatar", {
+                                required: "URL is required",
+                                minLength: { value: 4, message: "Valid URL is required" }
+                            })} type='text' name='avatar' placeholder='Image URL' color='rgba(255, 255, 255, 0.7)' />
+                            <FormErrorMessage>{errors.avatar?.message}</FormErrorMessage>
+                        </FormControl>
+
+                        <Button colorScheme='teal' type='button' mr={2} mt={4} onClick={handleBack}>
+                            Back
+                        </Button>
+                        <Button colorScheme='teal' type='submit' mt={4}>
+                            Submit
+                        </Button>
+                    </Box>
+                )}
+            </form>
+
+
+
         </Center>
     );
 }
